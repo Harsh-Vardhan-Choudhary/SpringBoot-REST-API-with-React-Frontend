f06aff9330dc0b1c2f240e60a2cadcb2117442cd
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-06-27 20:17:58 +0530

    changes

--- src/main/java/com/harsh/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/harsh/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -2,8 +2,11 @@ package com.harsh.rest.webservices.restfulwebservices.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +22,17 @@ public class TodoResource {
     public List<Todo> retrieveTodos(@PathVariable String username) {
         return todoService.findByUsername(username);
     }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
+        return todoService.findById(id);
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
 }
